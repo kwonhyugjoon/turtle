083568d2549492388034ef74412e5aa634753691
kwonhyugjoon <kwonhyugjoon58@gmail.com>
2025-08-07 14:56:17 +0900

    character move
    1

--- python.py
+++ python.py
@@ -1,8 +1,16 @@
-import turtle
-import random
+from pico2d import *
 
-for i in range(1,3+1):
-    turtle.penup()
-    turtle.goto(random.randint(-300,300),random.randint(-300,300))
-    turtle.pendown()
-    turtle.circle(random.randint(10,100))
+open_canvas()
+
+player = load_image('character.png')
+grass = load_image('grass.png')
+
+i = 0
+while(i < 800):
+    clear_canvas_now()
+    player.draw_now(i,90)
+    grass.draw_now(400,30)
+    i = i + 10
+    delay(0.2)
+
+close_canvas()
